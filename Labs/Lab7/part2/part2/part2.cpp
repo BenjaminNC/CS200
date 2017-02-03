f77f5364d62adbbe0baf6db6785bd7e3a35ff958
BenjaminNC <bncarpenter2@gmail.com>
2017-02-02 19:13:09 -0600

    Add files via upload

---
+++ Labs/Lab7/part2/part2/part2.cpp
@@ -0,0 +1,17 @@
+//Benjamin Carpenter
+#include <iostream>
+using namespace std;
+
+// Declare function here
+float AddTax(float dollar);
+int main()
+{
+	cout << "Price: $" << 9.99 << " \t with tax: $" << AddTax(9.99) << endl;
+	cout << "Price: $" << 19.95 << " \t with tax: $" << AddTax(19.95) << endl;
+	cout << "Price: $" << 10.00 << " \t with tax: $" << AddTax(10.00) << endl;
+	while (true);
+	return 0;
+}
+float AddTax(float dollar) {
+	return (dollar + (dollar * 0.12));
+}
